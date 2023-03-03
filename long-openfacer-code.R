3ab9016ee20f3b4025cb28a6424d4ccb8f84a291
tomato-tomato-tomato <79496523+tomato-tomato-tomato@users.noreply.github.com>
2023-03-03 16:46:10 +1100

    tidied + added emotion summary code
    Bypasses issue of OpenFaceR step not working - code for creating dataframes for count of each emotion added.

--- long-openfacer-code.R
+++ long-openfacer-code.R
@@ -6,30 +6,50 @@ remotes::install_github("davidecannatanuig/openFaceR")
 library(openfacer)
 library(tidyverse)
 library(pracma)
+library(purrr)
+library(dplyr)
 
-#1. Read in output from OpenFace (i.e., individual csv files): allows us to import all csv files contained in a given folder into object of class "faces", similar to a list. "Faces" is a list of tibles - each tibble represents output from one video. 
+#Read in output from openface (i.e., individual csv files): allows us to import all csv files contained in a given folder into object of class "faces", similar to a list. "Faces" is a list of tibbles - each tibble represents output from one video. 
+output <- read_face_csvs(output_dir = "/Users/manvisethi/FIT/cleancsvs/synced/") 
 
-output <- read_face_csvs(output_dir = "/Users/manvisethi/FIT/cleancsvs/") 
-#output <- output[1]
-
-#2. Filter out unsuccessful frames. Could potentially use this step to clean up CSV files also! eg. filter_face(timestamp < 180) Results can be stored in new filtered faces objects or can use pipes to complete subsequent steps. 
-#filtered_output <- filter_faces(output, success == 1)
-
-
-#Calculate emotions based on the appropriate action units. Adds a new column at the end of data frame 
-
+#Calculate emotions based on the appropriate action units. This will add a new column at the end of each tibble. 
 emotion_output <- output %>%
   openfacer::mutate_faces(happiness = if_else(AU12_c + AU06_c == 2, 1, 0),
                           sadness = if_else(AU01_c + AU04_c + AU15_c == 3, 1, 0),
                           fear = if_else(AU01_c + AU02_c + AU04_c + AU05_c + AU07_c + AU20_c + AU26_c == 7, 1, 0),
                           anger = if_else(AU04_c + AU05_c + AU07_c + AU23_c == 4, 1, 0))
 
-
-#3. Selects just the happiness variable from output2 and puts into a new file called output3 
-
+#Selects a specific emotion variable from emotion_output and creates a new tibble with timestamp, frame, success status, and emotion observation. 
 select_happiness <- select_faces(emotion_output,happiness) 
 select_sadness <- select_faces(emotion_output,sadness) 
 select_fear <- select_faces(emotion_output,fear) 
 select_anger <- select_faces(emotion_output,anger) 
 
+#Create summaries for each emotion 
+#anger
+anger_counts <- select_anger %>% 
+  map_dbl(~ sum(.x$anger == 1)) %>% 
+  tibble(name = names(select_anger), count = .)
+# print the resulting data frame
+anger_counts
 
+#fear
+fear_counts <- select_fear %>% 
+  map_dbl(~ sum(.x$fear == 1)) %>% 
+  tibble(name = names(select_fear), count = .)
+# print the resulting data frame
+fear_counts
+
+#sadness
+sadness_counts <- select_sadness %>% 
+  map_dbl(~ sum(.x$sadness == 1)) %>% 
+  tibble(name = names(select_sadness), count = .)
+# print the resulting data frame
+sadness_counts
+
+#happiness
+happiness_counts <- select_happiness %>% 
+  map_dbl(~ sum(.x$happiness == 1)) %>% 
+  tibble(name = names(select_happiness), count = .)
+# print the resulting data frame
+happiness_counts
